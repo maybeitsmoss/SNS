c73e466db645ae215f93c1615cf6a7ddead52a55
maybeitsmoss <ckelle25@pratt.edu>
2026-03-01 20:35:04 -0500

    ok

--- Assets/scripts/Conductor_2.cs
+++ Assets/scripts/Conductor_2.cs
@@ -31,7 +31,7 @@ public class Conductor_2 : MonoBehaviour
     public int endBar;
 
     private int difficulty;
-    public int difficultyChange;
+    public List<int> difficultyChange;
 
     void Start()
     {
@@ -101,22 +101,40 @@ public class Conductor_2 : MonoBehaviour
         {
             if(barCount % 2 == 0)
             {
+                if(barCount == difficultyChange[0])
+                {
+                    difficulty += 1;
+                }
+                else if (barCount == difficultyChange[1])
+                {
+                    difficulty += 1;
+                }
                 djTurn = false;
                 Debug.Log("Player's turn");
             }
             else if (barCount % 2 != 0)
             {
-                if(barCount >= difficultyChange)
+                if(barCount == difficultyChange[0])
                 {
                     difficulty += 1;
                 }
+                else if (barCount == difficultyChange[1])
+                {
+                    difficulty += 1;
+                }
+
                 djTurn = true;
                 Debug.Log("DJ's turn");
+
                 if (difficulty == 0)
                 {
                     StartCoroutine(Attack());
                 }
-                else
+                else if (difficulty == 1)
+                {
+                    StartCoroutine(GroovyAttack1());
+                }
+                else if (difficulty == 2)
                 {
                     StartCoroutine(FastAttack());
                 }
@@ -174,6 +192,112 @@ public class Conductor_2 : MonoBehaviour
         {
             Instantiate(exclaimPrefab, rightBottom.transform.position, Quaternion.identity);
         }
+    }
+
+    IEnumerator GroovyAttack1()
+    {
+        List <int> spawnList = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            int randomSpawn = Random.Range(1, 5);
+            spawnList.Add(randomSpawn);
+        }
+
+        Debug.Log(spawnList[0]);
+        makePopup(spawnList[0]);
+
+        yield return new WaitForSeconds(crochet * 2);
+
+        Debug.Log(spawnList[1]);
+        makePopup(spawnList[1]);
+
+        yield return new WaitForSeconds(crochet * 2);
+        
+        Debug.Log(spawnList[2]);
+        makePopup(spawnList[2]);
+
+        yield return new WaitForSeconds(crochet);
+
+        Debug.Log(spawnList[3]);
+        makePopup(spawnList[3]);
+
+        yield return new WaitForSeconds(crochet);
+
+        Debug.Log(spawnList[4]);
+        makePopup(spawnList[4]);
+
+        StartCoroutine(GroovySpawnEnemies(spawnList));
+
+    }
+
+     IEnumerator GroovySpawnEnemies(List<int> spawnHistory)
+    {
+        //yield return new WaitForSeconds(crochet);
+
+        List<GameObject> spawnPoints = new List<GameObject>();
+        for (int i = 0; i < spawnHistory.Count; i++)
+        {
+            if (spawnHistory[i] == 1)
+            {
+                int randomNum = Random.Range(1, 5);
+                if(randomNum == 4)
+                {
+                    Instantiate(footEnemyPrefab, leftTop.transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(enemyPrefab, leftTop.transform.position, Quaternion.identity);
+                }
+            }
+            else if (spawnHistory[i] == 2)
+            {
+                int randomNum = Random.Range(1, 5);
+                if(randomNum == 4)
+                {
+                    Instantiate(footEnemyPrefab, leftBottom.transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(enemyPrefab, leftBottom.transform.position, Quaternion.identity);
+                }
+            }
+            else if (spawnHistory[i] == 3)
+            {
+                int randomNum = Random.Range(1, 5);
+                if(randomNum == 4)
+                {
+                    Instantiate(footEnemyPrefab, rightTop.transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(enemyPrefab, rightTop.transform.position, Quaternion.identity);
+                }
+            }
+            else if (spawnHistory[i] == 4)
+            {
+                int randomNum = Random.Range(1, 5);
+                if(randomNum == 4)
+                {
+                    Instantiate(footEnemyPrefab, rightBottom.transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(enemyPrefab, rightBottom.transform.position, Quaternion.identity);
+                }
+            }
+            
+            if (i < 2)
+            {
+                yield return new WaitForSeconds(crochet * 2);
+            }
+            else
+            {
+                yield return new WaitForSeconds(crochet);
+            }
+            
+        }
+
     }
 
     IEnumerator FastAttack()
@@ -287,7 +411,7 @@ public class Conductor_2 : MonoBehaviour
             {
                 yield return new WaitForSeconds(crochet * 2);
             }
-            else
+            else if (difficulty == 2)
             {
                 yield return new WaitForSeconds(crochet);
             }
