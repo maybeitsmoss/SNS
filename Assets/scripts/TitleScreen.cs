9348ae2389220505211ed14417131139adea3468
MossOnMars <m0zz.0n.marz@gmail.com>
2026-03-05 03:29:50 -0500

    code made for scene load in

--- Assets/scripts/TitleScreen.cs
+++ Assets/scripts/TitleScreen.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGame()
     {
-        
-    }
+        SceneManager.LoadSceneAsync("tutorial");
+    }    
+
 }
